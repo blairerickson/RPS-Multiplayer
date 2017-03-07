e0462306bd7c9983736e0ecd401a53e839c4be0b
Blair Erickson <blair@jamwix.com>
2017-03-07 15:49:18 -0500

    RPS initialize interactjs and basic firebase chat

--- GameLogic.js
+++ GameLogic.js
@@ -3,17 +3,13 @@
 
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDAAqG02RyCj4h8wysgf-7KYHp0e95aI1A",
-    authDomain: "employee-demo-47a07.firebaseapp.com",
-    databaseURL: "https://employee-demo-47a07.firebaseio.com",
-    storstartdateBucket: "employee-demo-47a07.appspot.com",
-    messagingSenderId: "1042883643814"
+    apiKey: "AIzaSyDkJht_PrmbHzZZ_wOvPQVzaGBFVXNTvfA",
+    authDomain: "rps-multiplayer-384ab.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-384ab.firebaseio.com",
+    storageBucket: "rps-multiplayer-384ab.appspot.com",
+    messagingSenderId: "441630694834"
   };
   firebase.initializeApp(config);
-
-
-
-
 
 
     // Create a variable to reference the database.
@@ -21,22 +17,23 @@
 
     // Initial Values
     var name = "";
-    var role = "";
-    var startdate = 0;
-    var salary = "";
+    var chat = "";
 
+// selects the player for the round
 
-$(".fruitpicker li a").click( function() {
+$(".playpicker li a").click( function() {
+  console.log("playerpicker triggered");
    event.preventDefault();
-    var fruit = $(this).text();
-    console.log(fruit);
-    $("#LebowskiText").html(fruit + " has been selected.");
+    var player = $(this).text();
+    console.log(player);
+    $("#PlayerSelected").html(player + " has been selected.");
 
-    $("#LebowskiButton").val(fruit);
+    $("#LebowskiButton").val(player);
 });
 
 
-    // Capture Button Click
+ // Capture Button Click for chat
+ 
     $("#submit-data").on("click", function(event) {
       event.preventDefault();
 
@@ -44,24 +41,18 @@ $(".fruitpicker li a").click( function() {
 
       // Grabbed values from text boxes
       name = $("#name-input").val().trim();
-      role = $("#role-input").val().trim();
-      startdate = $("#startdate-input").val().trim();
-      salary = $("#salary-input").val().trim();
-      var fruit = $("#LebowskiButton").val();
-      console.log(fruit);
+      chat = $("#chat-input").val().trim();
+      console.log(chat);
 
       // Code for handling the push
       database.ref().push({
         name: name,
-        role: role,
-        startdate: startdate,
-        salary: salary,
-        fruit:fruit
+        chat: chat
       });
 
     });
 
-    // Firebase watcher + initial loader HINT: .on("value")
+    // Firebase watcher + initial loader  
     database.ref().on("value", function(snapshot) {
 
       // storing the snapshot.val() in a variable for convenience
@@ -102,8 +93,102 @@ $(".fruitpicker li a").click( function() {
           database.ref().orderByChild("startdate").on("child_added", function(childSnapshot) {
       		console.log("child added");
 
-             $("#EmployeeList").append("<div class='list-group-item active'>" + childSnapshot.val().name + "</div>" + "<div class='list-group-item'>" + childSnapshot.val().role + "</div>" + "<div class='list-group-item'> $" + childSnapshot.val().salary + "</div>" + "<div class='list-group-item'>" + childSnapshot.val().startdate + "</div>" + "<div class='list-group-item'> Started working here " + moment(childSnapshot.val().startdate).fromNow() + "</div><br>");
+             $("#ChatList").append("<div class='list-group-item active'>" + childSnapshot.val().name + "</div>" + "<div class='list-group-item'>" + childSnapshot.val().chat + "</div>" + moment(childSnapshot.val().chatdate).fromNow() + "</div><br>");
 
                  console.log(moment(childSnapshot.val().startdate).fromNow());
 
           });
+
+
+// Drag & Drop functions from interactjs
+
+interact('.draggable')  
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+  });
+
+  function dragMoveListener (event) {
+    console.log("event draggable target stuff is : " + event.target.val);
+    
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+// enable draggables to be dropped into this
+interact('.dropzone').dropzone({  
+  // Require a 50% element overlap for a drop to be possible
+  overlap: 0.50,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active');
+  },
+  ondragenter: function (event) {
+
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target');
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target');
+  },
+  ondrop: function (event) {
+    console.log("event target drop stuff is : " + event.target);
+    console.dir(event.relatedTarget.attributes.value.nodeValue);
+
+    var weapon = event.relatedTarget.attributes.value.nodeValue;
+
+
+     event.relatedTarget.textContent = 'Dropped';
+         if (weapon == "bomb")
+          {
+           $(event.relatedTarget).html('<img src="imgs/bomb-iconA.png" width="100%">');
+          }
+        if (weapon == "scissors")
+          {
+           $(event.relatedTarget).html('<img src="imgs/scissors-iconA.png" width="100%">');
+          }
+
+
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+  }
+});
+
+
+// Protocode: 
+$(document).ready(function(){
+
+  $("#box-1").html('<img src="imgs/bomb-icon.png" width="100%">');
+
+  });
